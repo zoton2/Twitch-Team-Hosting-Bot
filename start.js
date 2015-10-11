dcf00c0c3beedf077ead76ff40f901fe94b41db7
zoton2 <zoton2@gmail.com>
2015-10-11 14:50:23 +0100

    Added the way of finding a channel to host
    Also renamed the connections file to init to be more descriptive to what
    it actually does now.

--- start.js
+++ start.js
@@ -1,10 +1,14 @@
 // Referencing other files.
-var connections = require('./lib/connections');
-var hosting = require('./lib/hosting');
+var globalVars = require('./lib/global-vars');
+var init = require('./lib/init');
+var hostingFuncs = require('./lib/hosting');
 
-// Does the initial connection setting up stuff (also joins channels).
-connections.setUpConnections(function() {
-	hosting.getOnlineChannels('example', function(onlineChannels) {
-		console.log(onlineChannels);
-	});
+// Does the initial connection and setting up stuff.
+init.setUp(function() {
+	// Starts off by selecting a channel from each team needed.
+	for (var key in globalVars.teamChannels) {
+		if (globalVars.teamChannels.hasOwnProperty(key)) {
+			hostingFuncs.chooseChannel(key);
+		}
+	}
 });
