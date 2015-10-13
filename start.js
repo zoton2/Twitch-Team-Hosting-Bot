058e88bbbcdb09cb705b05b319eff4ff9d91c545
zoton2 <zoton2@gmail.com>
2015-10-13 21:26:52 +0100

    Added starting code for chat commands
    Also changed the way IRC client instances are stored to make it easier
    for future code.

--- start.js
+++ start.js
@@ -2,12 +2,14 @@
 var globalVars = require('./lib/global-vars');
 var init = require('./lib/init');
 var hostingFuncs = require('./lib/hosting');
+var chatCommandFuncs = require('./lib/chat-commands');
 
 // Does the initial connection and setting up stuff.
 init.setUp(function() {
 	// Starts off by selecting a channel from each team needed.
 	for (var key in globalVars.teamChannels) {
 		if (globalVars.teamChannels.hasOwnProperty(key)) {
+			chatCommandFuncs.setUpListening(key);
 			hostingFuncs.chooseChannel(key);
 		}
 	}
