a315e56fa187711cd51d40debc57a04301ed7572
zoton2 <zoton2@gmail.com>
2015-10-15 00:49:24 +0100

    Added a current host time command
    Also changed some minor stuff like variable names to be shorter.

--- start.js
+++ start.js
@@ -7,8 +7,8 @@ var chatCommandFuncs = require('./lib/chat-commands');
 // Does the initial connection and setting up stuff.
 init.setUp(function() {
 	// Goes through all of the teams at the start to kick things off.
-	for (var key in globalVars.teamChannels) {
-		if (globalVars.teamChannels.hasOwnProperty(key)) {
+	for (var key in globalVars.channels) {
+		if (globalVars.channels.hasOwnProperty(key)) {
 			chatCommandFuncs.setUpListening(key);
 			hostingFuncs.chooseChannel(key);
 		}
