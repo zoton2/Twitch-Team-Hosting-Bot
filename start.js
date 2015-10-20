70281ab7fb8ac156435c1938afe058d849dd2623
zoton2 <zoton2@gmail.com>
2015-10-20 06:06:24 +0100

    Added/changed a bunch of things
    - Started adding a custom logger (right now it just prints to the
    console) and added a few places where messages are logged in this way.
    - Added toggle to the login-details file for "debug"; right now this
    just toggles tmi.js debug.
    - Fixed a bug where the bot would crash if someone on the team didn't
    have their game set.
    - Added an extra connection for whispers (still need to add parts that
    use it).
    - Simplified the process of calculating the hosted time.
    - Other minor stuff.

--- start.js
+++ start.js
@@ -1,8 +1,11 @@
 // Referencing other files.
+var loggingFuncs = require('./lib/logging');
 var globalVars = require('./lib/global-vars');
 var init = require('./lib/init');
 var hostingFuncs = require('./lib/hosting');
 var chatCommandFuncs = require('./lib/chat-commands');
+
+loggingFuncs.logMessage(null, 'Starting up...');
 
 // Does the initial connection and setting up stuff.
 init.setUp(function(autoStartList) {
