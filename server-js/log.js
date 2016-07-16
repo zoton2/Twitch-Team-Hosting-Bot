ab06d63a30cb42c0dabd7837ee8160ad0c18ba05
zoton2 <zoton2@gmail.com>
2016-07-17 00:50:36 +0100

    Some minor corrections

--- server-js/log.js
+++ server-js/log.js
@@ -3,7 +3,7 @@ var logLines = $('#log-lines').html().split('<br>');
 var newLogLines = [];
 
 // Go through each line and replace the timestamp with one that is in the viewer's local time.
-for (var i = 0; i < logLines.length; i++){
+for (var i = 0; i < logLines.length; i++) {
     var localTime = moment(moment.utc(logLines[i].substr(4, 19)).toDate()).format('YYYY-MM-DD HH:mm:ss');
 	newLogLines.push('<b>[' + localTime + ']</b> ' + logLines[i].substr(29));
 }
