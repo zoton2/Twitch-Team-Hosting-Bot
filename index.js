f92bef86cdffc122666fe770dad766cc4283eb4a
zoton2 <zoton2@gmail.com>
2016-09-19 02:07:47 +0100

    Disconnect TPS on close and renaming dumb vars
    There were a few files that I reference where I called the variable
    "[whatever]Funcs" and I realised it was kind of dumb so renamed them.

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
 // Referencing other files.
-var loggingFuncs = require('./lib/logging');
+var logging = require('./lib/logging');
 var globalVars = require('./lib/global-vars');
 var init = require('./lib/init');
-var hostingFuncs = require('./lib/hosting');
-var chatCommandFuncs = require('./lib/chat-commands');
+var hosting = require('./lib/hosting');
+var chatCommands = require('./lib/chat-commands');
 var githubAPI = require('./lib/github-api');
 
-loggingFuncs.logMessage(null, 'Starting up (version ' + globalVars.version + ').');
+logging.logMessage(null, 'Starting up (version ' + globalVars.version + ').');
 githubAPI.checkForNewVersion();
 
 // Does the initial connection and setting up stuff.
 init.setUp(function(autoStartList) {
-	loggingFuncs.logMessage(null, 'All teams set up and connected.');
+	logging.logMessage(null, 'All teams set up and connected.');
 	
 	// Goes through all of the teams at the start to kick things off; will start the hosting if set to do this.
 	for (var team in globalVars.channels) {
 		if (globalVars.channels.hasOwnProperty(team)) {
 			globalVars.adminCommandsActive[team] = true;
-			chatCommandFuncs.setUpListening(team);
+			chatCommands.setUpListening(team);
 			if (autoStartList.indexOf(team) >= 0) {
-				hostingFuncs.turnOnHosting(team, function(error, message) {
+				hosting.turnOnHosting(team, function(error, message) {
 					if (!error) {
 						// could send the message at some point
 					}
